d144ba8913bcc4eae91b479378eefbdbc2968a0b
liunanchenFYJJ <liunan_chen@163.com>
2018-06-20 17:14:29 +0800

    update Login

---
+++ src/components/Cus-alert.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-show="show" class="alert alert-danger" role="alert">
+        <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    // data: function () {
+    //     return {
+    //         ok: 'true'
+    //     }
+    // },
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,33 +1,74 @@
 <template>
-    <div class="login jumbotron">
-        <div class="container">
+    <div class="row jumbotron">
+        <div class="container col-md-4 col-xs-6 col-md-offset-4 col-xs-offset-3">
             <form>
                 <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                    <label for="email">邮箱</label>
+                    <input type="email" class="form-control" v-model="email" @change="validateEmail" placeholder="you@example.com">
+                    <Cus-alert v-bind:show="ok">Enter a valid email address</Cus-alert>
                 </div>
                 <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputFile">File input</label>
-                    <input type="file" id="exampleInputFile">
-                    <p class="help-block">Example block-level help text here.</p>
+                    <label for="password">密码</label>
+                    <input type="password" class="form-control" v-model="password" @change="validatePassword" placeholder="password">
+                    <Cus-alert v-bind:show="ok1">密码错误</Cus-alert>
                 </div>
                 <div class="checkbox">
                     <label>
-                    <input type="checkbox"> Check me out
+                    <input type="checkbox" v-model="remeberMe">记住我
                     </label>
                 </div>
-                <button type="submit" class="btn btn-default">Submit</button>
+                <button type="submit" class="btn btn-info" @click="submit">提交</button>
             </form>
         </div>
     </div>
 </template>
-<style scoped>
-form {
-    margin: 0 300px 0 300px;
+<script>
+import CusAlert from '@/components/Cus-alert'
+export default {
+    name: 'Login',
+    components: { CusAlert },
+    data: function () {
+        return {
+            email: 'example.com',
+            password: '123456',
+            remeberMe: true,
+            ok: false,
+            ok1: false,
+        }
+    },
+    // props: {
+    //     email: Number,
+    //     password: {
+    //         validator: function (value) {
+    //             return value > 100;
+    //         }
+    //     }
+    // },
+    methods: {
+        submit: function (e) {
+            e.preventDefault();
+            console.log('submit');
+        },
+        validateEmail: function () {
+            var reg = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.[a-zA-Z0-9]{2,6}$/;
+            if (reg.test(this.email)) {
+                console.log('success');
+            } else {
+                this.ok = true;               
+            }
+        },
+        validatePassword: function () {
+            if (this.password.length >= 6) {
+                console.log('success');
+            } else {
+                this.ok1 = true;                
+            }
+        }
+    }
 }
+</script>
+
+<style scoped>
+
 </style>
 
