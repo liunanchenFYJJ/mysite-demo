e6e0ea84dbd5a20c2a3322afa4aa295328a15a98
liunanchenFYJJ <liunan_chen@163.com>
2018-06-23 18:59:10 +0800

    update Calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,12 +2,12 @@
     <div class="calendar">
         <div class="month">      
             <ul>
-                <li class="prev">❮</li>
-                <li class="next">❯</li>
+                <li class="prev" @click="changeMonth">❮</li>
+                <li class="next" @click="changeMonth">❯</li>
                 <li style="text-align:center">{{month}}<br>
                     <span style="font-size:18px">{{year}}</span>
                     <span v-if="leapYear">闰年</span>
-                    <span v-else-if="!leapYear">平年</span>
+                    <span v-else>平年</span>
                 </li>
             </ul>
         </div>
@@ -23,9 +23,11 @@
         </ul>
 
         <ul class="days">  
-            <li v-for="i in (this.date)" v-bind:key="i">{{i}}</li>
-            <li><span class="active">{{date}}</span></li>
-            <li v-for="i in (30-this.date)" v-bind:key="i + 100">{{date + i}}</li>
+            <li v-for="i in this.firstday" :key="i + 1000"></li>
+            <li @click="selectDate" v-for="i in this.monthDays" :key="i">
+                <span class="active" v-if="i == date">{{i}}</span>
+                <span v-else>{{i}}</span>
+            </li>
         </ul>
     </div>
 </template>
@@ -36,14 +38,16 @@ export default {
             year: '',
             leapYear: '',   //判断闰年
             month: '',
+            monthDays: '',  //当月的天数
             date: '',
-            day: ''
+            firstday: ''
         }
     },
     created: function () {
-        this.getYear();
-        this.getMonth();
-        this.getDay();
+        this.getYear();     //获取年份，判断奇偶年
+        this.getMonth();    //获取月份，判断每月天数
+        this.getDay();      //获取今天日期
+        this.getWeek(this.year, this.month, 1);     //判断每月1号星期几
     },
     methods: {
         getYear: function () {
@@ -55,15 +59,62 @@ export default {
                 this.leapYear = false
             }
         },
-        getMonth: function () {
+        getMonth: function (month) {
             var date = new Date();
-            this.month = date.getMonth() + 1;
+            this.month = (date.getMonth() + 1) || month;
+            console.log(month);
+            switch (this.month) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 9:
+                case 11:
+                    return this.monthDays = 31;
+                    break;
+                case 4:
+                case 6:
+                case 8:
+                case 10:
+                case 12:
+                    return this.monthDays = 30;
+                    break;
+                default:
+                    if (this.leapYear == true) {
+                        return this.monthDays = 29;                    
+                    } else {
+                        return this.monthDays = 28;                    
+                    }
+                    break;
+            }
         },
         getDay: function () {
             var date = new Date();
             this.date = date.getDate();  
-            this.day = date.getDay();
-            console.log(this.day);
+        },
+        getWeek: function (year, month, date) {
+            var firstday = new Date(year, month - 1, date);
+            this.firstday = firstday.getDay();
+        },
+        selectDate: function (e) {
+            if (!e.target.className) {
+                e.target.className = 'active1';
+            }
+        },
+        changeMonth: function (e) {
+            if (e.target.className == 'prev') {
+                this.month--;
+                if (this.month < 1) {
+                    this.year -= 1;
+                    this.month = 12;
+                }
+            } else {
+                this.month++;
+                if (this.month > 12) {
+                    this.year += 1;
+                    this.month = 1;
+                }
+            }
         }
     }
 }
@@ -97,6 +148,11 @@ ul {list-style-type: none;}
     padding-top: 10px;
 }
 
+.month .prev:hover, .month .next:hover {
+    cursor: pointer;
+    color: rebeccapurple;
+}
+
 .month .next {
     float: right;
     padding-top: 10px;
@@ -124,7 +180,7 @@ ul {list-style-type: none;}
 .days li {
     list-style-type: none;
     display: inline-block;
-    width: 12%;
+    width: 13.2%;
     text-align: center;
     margin-bottom: 5px;
     font-size:12px;
@@ -136,7 +192,11 @@ ul {list-style-type: none;}
     background: #1abc9c;
     color: white !important
 }
-
+.active1 {
+    padding: 5px;
+    background:red;
+    color: white !important
+}
 /* Add media queries for smaller screens */
 @media screen and (max-width:720px) {
     .weekdays li, .days li {width: 13.1%;}
