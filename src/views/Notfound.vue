6683fce0cdc63bbaec63c7b00bd34a797f549999
liunanchenFYJJ <liunan_chen@163.com>
2018-06-25 17:33:18 +0800

    update 404

--- src/views/Notfound.vue
+++ src/views/Notfound.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>
-        404Notfound
+    <div class="notfound row">
+        <div class="col-md-8 col-md-offset-2">
+            <h1 class="text-danger">&nbsp;<span class="glyphicon glyphicon-remove-circle"></span>&nbsp;404&nbsp;<span class="text-info h3">Page lost~</span></h1>
+            <img src="../assets/notfound.gif" alt="notfound-page">
+            <h3>Or you can <span class="h2"><a href="/layout/home">back to home<span class="glyphicon glyphicon-plane"></span></a></span></h3>
+        </div>
     </div>
 </template>
+<style scoped>
+.notfound {
+    height: 100%;
+    width: 100%;
+    margin: 0 0 0 0;
+    font-family: fantasy;    
+}
+a:hover {
+    text-decoration-line: none;
+    cursor: pointer;
+}
+</style>
+
