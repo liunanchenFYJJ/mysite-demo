50e0a1044fde663138e4195d56bc129b2e85057b
liunanchenFYJJ <liunan_chen@163.com>
2018-06-26 17:23:04 +0800

    add d3kittimeline

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="blog row">
-        <div class="col-md-10">main</div>
+        <div class="col-md-10">main
+            <div id="timeline"></div>
+            <button @click="add">add</button>
+        </div>
         <div class="col-md-2">
             <calendar />
         {{selectmsg}}
@@ -11,15 +14,30 @@
 </template>
 <script>
 import Calendar from '@/components/Calendar'
+import d3KitTimeline from 'd3kit-timeline'
 import Bus from '@/bus'
 export default {
     components: { Calendar },
     data: function () {
         return {
-            selectmsg: ['ads']
+            selectmsg: ['ads'],
+            data: [
+                // {time: new Date(1977, 4,25), episode: 4, name: 'A New Hope'},
+                // {time: new Date(1980, 4,17), episode: 5, name: 'The Empire Strikes Back'},
+                // {time: new Date(1984, 4,25), episode: 6, name: 'Return of the Jedi'},
+                // {time: new Date(1999, 4,19), episode: 1, name: 'The Phantom Menace'},
+                // {time: new Date(2002, 4,16), episode: 2, name: 'Attack of the Clones'},
+                // {time: new Date(2005, 4,19), episode: 3, name: 'Revenge of the Sith'},
+                {time: new Date(2014,11,18), episode: 7, name: 'The Force Awakens'},
+                {time: new Date(2015,1,18), episode: 7, name: 'The Force Awakens'},
+                {time: new Date(2015,6,18), episode: 7, name: 'The Force Awakens'},
+                {time: new Date(2015,11,19), episode: 7, name: 'The Force Awakens'},
+            ],
+            chart: {}
         }
     },
     mounted: function () {
+        this.timeline();
         Bus.$on('msg', function (data) {
             var self = this;
             self.selectmsg = data;
@@ -36,16 +54,41 @@ export default {
             e.preventDefault();
             var self = this;            
             console.log(self.selectmsg);
+        },
+        timeline: function () {
+            this.chart = new d3KitTimeline('#timeline', {
+                // direction: 'up',    //标签方向
+                // initialWidth: 400,
+                // initialHeight: 250,
+                formatAxis: axis => axis.ticks(0),  //设置轴线分割点数
+                textFn: function(d){
+                    return d.time.getFullYear() + ' - ' + d.name;
+                },
+                dotColor: 'red',        //标签点颜色
+                labelBgColor: 'grey',    //标签背景色
+                labelTextColor: '#fff',  //标签文字颜色
+                linkColor: 'red',       //标签线颜色
+                labelPadding: {left: 10, right: 10, top: 2, bottom: 2}
+            });
+
+            this.chart.data(this.data);
+            this.chart.on('labelClick', function(d){
+                console.log(d.data.name)
+            });
+        },
+        add: function () {
+            this.data.push({time: new Date(Math.random()*100+1900,11,18), episode: 7, name: 'XXXX '});
+            this.chart.data(this.data);
         }
     },
-    beforeCreate: function(){console.log('beforeCreate')},
-    created: function(){console.log('Create')},
-    beforeMount: function(){console.log('beforeMount')},
-    mounted: function(){console.log('Mount')},
-    beforeUpdate: function(){console.log('beforeUpdate')},
-    updated: function(){console.log('Update')},
-    beforeDestroy: function(){console.log('beforeDestroy')},
-    destroyed: function(){console.log('Destroy')}
+    // beforeCreate: function(){console.log('beforeCreate')},
+    // created: function(){console.log('Create')},
+    // beforeMount: function(){console.log('beforeMount')},
+    // mounted: function(){console.log('Mount')},
+    // beforeUpdate: function(){console.log('beforeUpdate')},
+    // updated: function(){console.log('Update')},
+    // beforeDestroy: function(){console.log('beforeDestroy')},
+    // destroyed: function(){console.log('Destroy')}
 }
 </script>
 
